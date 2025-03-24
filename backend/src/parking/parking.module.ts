bc915c6fe09d59248648c5899b534aa7512d5caf
AmanBarar <barar.amanjeet.aman@gmail.com>
2025-03-24 10:35:13 +0530

    docs(module): add coments to parking module

--- backend/src/parking/parking.module.ts
+++ backend/src/parking/parking.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ParkingController } from './parking.controller';
 import { ParkingService } from './parking.service';
 
+// Module class for the parking lot module
 @Module({
     controllers: [ParkingController],
     providers: [ParkingService]
